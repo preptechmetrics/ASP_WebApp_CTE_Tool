0740cbcaf0cdf0815935f1421d4ebec66040ed09
preptechmetrics <preptechmetrics@gmail.com>
2023-03-12 16:04:33 -0400

    Add project files.

---
+++ Controllers/TeachingFieldController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Web.Http;
+using TeachFieldLookup.Models;
+using HttpGetAttribute = System.Web.Http.HttpGetAttribute;
+
+namespace TeachFieldLookup.Controllers
+{
+    public class TeachingFieldController : ApiController
+    {
+        [HttpGet]
+        [Microsoft.AspNetCore.Mvc.Route("api/teachingfield/{teachFieldCode}")]
+        public IHttpActionResult GetTeachingField(string teachFieldCode)
+        {
+            string filePath = @"C:\path\to\CTE_Codes_Project.xlsx";
+            ExcelData excelData = new ExcelData();
+            Dictionary<string, Dictionary<string, string>> dict = excelData.GetDictionary(filePath);
+
+            if (dict.ContainsKey(teachFieldCode))
+            {
+                Dictionary<string, string> subDict = dict[teachFieldCode];
+                return Ok(new
+                {
+                    Subject = subDict["Subject"],
+                    Credential = subDict["Credential"],
+                    TeachField = subDict["TeachField"],
+                    TeachFieldName = subDict["TeachFieldName"]
+                });
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ Models/ExcelData.cs
@@ -0,0 +1,43 @@
+﻿using OfficeOpenXml;
+using System.ComponentModel;
+
+namespace TeachFieldLookup.Models
+{
+    public class ExcelData
+    {
+        public Dictionary<string, Dictionary<string, string>> GetDictionary(string filePath)
+        {
+            FileInfo fileInfo = new FileInfo(filePath);
+            ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
+            using (ExcelPackage package = new ExcelPackage(fileInfo))
+            {
+                ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
+                int rowCount = worksheet.Dimension.Rows;
+
+                Dictionary<string, Dictionary<string, string>> dict = new Dictionary<string, Dictionary<string, string>>();
+
+                for (int row = 2; row <= rowCount; row++)
+                {
+                    string key = worksheet.Cells[row, 1].Value.ToString();
+                    string subject = worksheet.Cells[row, 2].Value.ToString();
+                    string credential = worksheet.Cells[row, 3].Value.ToString();
+                    string teachField = worksheet.Cells[row, 4].Value.ToString();
+                    string teachFieldName = worksheet.Cells[row, 5].Value.ToString();
+
+                    if (dict.ContainsKey(teachField))
+                    {
+                        dict[teachField].Add(key, subject + "|" + credential + "|" + teachFieldName);
+                    }
+                    else
+                    {
+                        Dictionary<string, string> subDict = new Dictionary<string, string>();
+                        subDict.Add(key, subject + "|" + credential + "|" + teachFieldName);
+                        dict.Add(teachField, subDict);
+                    }
+                }
+
+                return dict;
+            }
+        }
+    }
+}
